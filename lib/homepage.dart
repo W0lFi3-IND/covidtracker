49c81ae59612b7fa975a9ec0593c173ac45c2c06
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-04-24 01:10:13 +0530

    second commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart'as http;
 class HomePage extends StatefulWidget
@@ -30,6 +31,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      bottomNavigationBar: b,
       body: Center(
         child: res!=null ?
         ListView.builder(
@@ -40,14 +42,88 @@ class _HomePageState extends State<HomePage> {
                 title:Card(
                   child: Container(
                     width: 300,
-                    height: 200,
+                    height: 180,
                     child: Column(
                       children: <Widget>[
-                        Text("${datas["state"]}")
+                        Padding(
+                          padding: const EdgeInsets.only(bottom:5.0,top:10),
+                          child: Text("${datas["state"]}",style: TextStyle(
+                            color: Color.fromRGBO(0, 165, 82,100),
+                              fontWeight: FontWeight.w800,
+                            fontSize: 20
+                          ),),
+                        ),
+
+                        Divider(
+                          color: Colors.black,
+                        ),
+                        Row(
+                          mainAxisAlignment:MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Text("Active",style: TextStyle(
+                                color:Colors.black,
+                                fontWeight: FontWeight.w500
+                            ),),
+                            Text("Confirmed",style: TextStyle(
+                                color:Colors.black,
+                                fontWeight: FontWeight.w500
+                            ),),
+                            Text("Recovered",style: TextStyle(
+                                color:Colors.black,
+                                fontWeight: FontWeight.w500
+                            ),),
+                            Text("Death",style: TextStyle(
+                                color:Colors.red,
+                                fontWeight: FontWeight.w500
+                            ),),
+                          ],
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:28.0),
+                    child: Row(
+                      mainAxisAlignment:MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text("${datas["active"]}",style: TextStyle(
+                            color:Colors.orangeAccent,
+                            fontWeight: FontWeight.w500,
+                          fontSize: 16
+                        ),),
+                        Text("${datas["confirmed"]}",style: TextStyle(
+                            color:Colors.deepOrange,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16
+                        ),),
+                        Text("${datas["recovered"]}",style: TextStyle(
+                            color:Colors.green,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16
+                        ),),
+                        Text("${datas["deaths"]}",style: TextStyle(
+                            color:Colors.red,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16
+                        ),),
                       ],
                     ),
+
+                  ),
+                        Padding(
+                          padding: const EdgeInsets.only(top:30.0,left: 10),
+                          child: Row(
+                            children: <Widget>[
+                              Text("${datas["lastupdatedtime"]}",style: TextStyle(
+                                  color:Colors.black,
+                                  fontWeight: FontWeight.w100,
+                                  fontSize: 16
+                              ),),
+                            ],
+                          ),
+                        ),
+
+                ],
                   ),
                 )
+              )
               );
             } )
             :CircularProgressIndicator(),
@@ -55,3 +131,4 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
